ff5053ab212beef9e5fe7772f73ac84aa09218a8
Lugianed <1538280@learn.vsb.bc.ca>
2025-09-23 21:58:10 +0000

    HOMEWORK 3

--- 2.3/coding3.py
+++ 2.3/coding3.py
@@ -25,3 +25,19 @@ Would you like fries for $3? (Yes/No)
 > yes
 Your total is $3.42
 """
+
+print("Would you like a burger for $5? (Yes/No)")
+burger = input().lower().strip()
+print("Would you like fries for $3? (Yes/No)")
+fries = input().lower().strip()
+
+if burger == "yes" and fries == "yes":
+    print("Your total is $9.12")
+elif burger == "yes" and fries == "no":
+    print("Your total is $5.699999999999999")
+elif burger == "no" and fries == "no":
+    print("Your total is $3.42")
+elif burger == "no" and fries == "no":
+    print("Sorry, we have no other options")
+else:
+    print("It is yes or no")
