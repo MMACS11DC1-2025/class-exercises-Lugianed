76acb5d7c9dbb3bb0c628f4f3ba04bc8b82048f2
Lugianed <1538280@learn.vsb.bc.ca>
2025-10-02 21:09:36 +0000

    weqw

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -6,3 +6,30 @@ You must use user input to add interactivity to the program.
 You must design your algorithm in English first, then translate it to Python code.
 Test as you go! Describe in your comments what steps you took to test your code.
 """
+
+# Opens the file up and reads the line with my information
+# Asks the user for their imput on the same questions that I answered
+# Gets those imputs and prints out a fraction score of what we had simular and a message depending on the score
+
+
+# Opens the file and reads it one line at a time
+file = open("2.4/responses.csv")
+file.readline()
+
+
+for i in range(2):
+    file.readline()
+
+me = file.readline().strip().split(",")
+
+
+for line in file:
+
+    digit = input("What is your favourite digit (1-10): ").strip().lower()
+    pet = input("What is your favourit pet: ").strip().lower()
+    subject = input("What is your favourite subject: ").strip().lower()
+    play = input("What is your favourite sport to play: ").strip().lower()
+    watch = input("What is your favourite sport to watch: ").strip().lower()
+    music = input("What is your favourite music genre: ").strip().lower()
+    movie = input("What is your favourite movie genre: ").strip().lower()
+    movie = input("What is your favourite movie genre: ").strip().lower()
