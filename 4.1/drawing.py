4002aa2c52eb554b108ccf2fd2ce4f27868796fd
Lugianed <1538280@learn.vsb.bc.ca>
2025-10-15 21:54:18 +0000

    hw

--- 4.1/drawing.py
+++ 4.1/drawing.py
@@ -4,3 +4,36 @@ Explore the turtle drawing package to create an interactive drawing.
 It should use a while loop.
 Your program should also include at least one function you’ve made yourself 
 """
+
+import turtle
+t = turtle.Turtle()
+
+def circle():
+  
+  t.penup()
+  t.goto(0, 0)
+  t.pendown()
+  t.circle(67)
+  
+
+def tri():
+	t.forward(100)
+	t.left(120)
+	t.left(120)
+	t.forward(100)
+	t.right(120)
+	t.forward(100)
+	t.penup()
+  
+ 
+while True:
+  command = input("Do you want a triangle or circle?").lower()
+  
+  if command == "triangle":
+    tri()
+    break
+  elif command == "circle":
+    circle()
+    break
+  else:
+    print("I don't understand")
