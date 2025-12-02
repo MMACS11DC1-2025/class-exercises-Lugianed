67a3c0c64ede879746d2fca64d3af70ee4c5102f
Lugianed <1538280@learn.vsb.bc.ca>
2025-12-02 21:11:57 +0000

    sdfsdfdss

---
+++ 6.7/imgexplorer.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import time
+
+pink_color = 0
+
+image_list = ("pinkflower1.webp", "pinkflower2.webp", "pinkflower3.webp", "pinkflower4.webp", "pinkflower5.webp"
+              , "pinkflower6.webp", "pinkflower7.webp", "pinkflower8.webp", "pinkflower9.webp", "pinkflower10.webp")
+for image in image_list:
+    file = Image.open("6.7/" + image)
+    img = file.load()
+
+    width = file.width
+    height = file.height
+
+    def is_target_color(r, g, b):
+        if 200 <= r >= 255 and 100 <= g >= 200 and 120 <= b >= 200:
+            return True
+        return False
+    for x in range(width):
+        for y in range(height):
+            r = img[x, y][0]
+            g = img[x, y][1]
+            b = img[x, y][2]
+        if is_target_color(r, g, b):
+            pink_color += 1
+
+print("Number of images with pink color:", pink_color)
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+1. I will be scanning flowers for my main theme.
+2. My code will be detecting if the flowers are pink.
+3. There will be a function that goes through all of the pixels to look if there is pink. Pink
+is usually not found in nature besides flowers, so if it detects pink, it will most likely be
+a flower.
+
+
+
+
+
+
 
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-2972f46106e565e64193e422d61a12cf1da4916b45550586e14ef0a7c637dd04.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=20617640)
 
