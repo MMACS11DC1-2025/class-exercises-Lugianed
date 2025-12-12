56e39ac8d4f881000abe0941c17b14e937ee3efb
Lugianed <1538280@learn.vsb.bc.ca>
2025-12-12 20:22:40 +0000

    asda

--- 6.7/imgexplorer.py
+++ 6.7/imgexplorer.py
@@ -4,11 +4,16 @@ import time
 count = 0
 final = 0
 def is_target_color(r, g, b):
-    if (100 <= r >= 255 and 7 <= g >= 150 and 120 <= b >= 200) and (3 <= r >= 155 and 5 <= g >= 180 and 0 <= b >= 60):
+    if 90 <= r >= 255 and 7 <= g >= 150 and 120 <= b >= 200:
+        print(r, g, b)
         return True
     return False
-
-image_list = ("pinkflower1.webp", "pinkflower2.webp", "pinkflower3.webp", "pinkflower4.webp", "pinkflower5.webp",
+def is_another_color(r, g, b):
+    if 3 <= r >= 155 and 5 <= g >= 180 and 0 <= b >= 60:
+        print(r, g, b)
+        return True
+    return False
+image_list = ("pinkflower1.webp", "pinkflower2.webp", "pinkflower3.webp", "pinkflower11.jpg", "pinkflower5.webp",
               "pinkflower6.webp", "pinkflower7.webp", "pinkflower8.png", "pinkflower9.png", "pinkflower10.png")
 for image in image_list:
     file = Image.open("6.7/" + image)
@@ -22,12 +27,11 @@ for image in image_list:
             r = img[x, y][0]
             g = img[x, y][1]
             b = img[x, y][2]
-            if is_target_color(r, g, b):
+            if is_target_color(r, g, b) and is_another_color(r, g, b):
                 pink_color += 1
-                print("Number of images with pink color:", pink_color)
+                print(x, y)
                 print(image)
         if pink_color == 1:
             final += 1
             break
-
 print(final)
