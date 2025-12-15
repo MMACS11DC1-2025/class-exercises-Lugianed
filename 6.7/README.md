2c75216a57d8931ae801a5a3804709d509aafed3
Lugianed <1538280@learn.vsb.bc.ca>
2025-12-15 05:55:34 +0000

    FINISHED

--- 6.7/README.md
+++ 6.7/README.md
@@ -1,3 +1,16 @@
+PROJECT OVERVIEW:
 1. It looks to see how much pink is in the fields of the flowers.
 2. It will detect the amount of pink and make it into a percent for the person using the program to see.
 3. It will just look for how much pink is in each picture and print.
+
+TESTING AND ROBUSTNESS:
+
+
+PERFORMANCE ANALYSIS:
+
+
+
+CHALLENGES FACED:
+One of the most major challenge I had to overcome for this project was adjusting the tolerance for pink detection.
+At first, it would output some pictures with 1.6% etc, but after I kept on fine tuning it, it eventually came to be
+what it is now. One of the most important parts of fine tuning this was finding the in between part of the r, g, b.
--- 6.7/imgexplorer.py
+++ 6.7/imgexplorer.py
@@ -1,59 +1,98 @@
 from PIL import Image
 import time
 
+# master list of all pink percentages
 picture_percents = []
-def is_target_color(r, g, b): # checks if it is pink
-    if 150 <= r >= 220 and 20 <= g >= 90 and 100 <= b >= 180:
+
+# checks if it is pink
+def is_target_color(r, g, b):
+    if r >= 115 and b >= 65 and g <= 185:
         return True
-    return False
+    else:
+        return False
+    
+# masterlist of images
 image_list = ("pink1.webp", "pink2.webp", "pink3.webp", "pink4.webp", "pink5.webp",
               "pink6.webp", "pink7.webp", "pink8.webp", "pink9.webp", "pink10.webp")
+
+# start of timing
+pixel_analysis_start = time.time()
+
+# goes through each image
 for image in image_list:
     file = Image.open("6.7/" + image)
     img = file.load()
-
     width = file.width
     height = file.height
+
+    # list of pink pixels in current image and restarts for the next
     pink_pixels = []
+
+    # goes through each pixel in the images
     for x in range(width):
         for y in range(height):
             r = img[x, y][0]
             g = img[x, y][1]
             b = img[x, y][2]
+            # checks if pink condition is true
             if is_target_color(r, g, b):
                 pink_pixels.append((img, (x, y)))
+
     # calculates percentage of pink pixels
     num_pink = len(pink_pixels)
-    total_pixels = width*height
+    total_pixels = width * height
     pink_ratio = num_pink / total_pixels
     pink_percent = pink_ratio * 100
-    report = (image) + " Is {:.2f}% pink.".format(pink_percent)
+    
+    # the thing that user sees
+    report = image + " has {:.2f}% pink.".format(pink_percent) # CHANGE FILE NAMES TO START WITH CAPITAL LETTERS
     print(report)
+
+    # adds to master list
     pink_round = round(pink_percent)
     picture_percents.append(pink_round)
+# finishes timing
+pixel_analysis_end = time.time()
+pixel_analysis_time = pixel_analysis_end - pixel_analysis_start
+print("Anaylsis through all pixels took {:.3f} seconds to look through.".format(pixel_analysis_time))
 
 # selection sort
-for i in range(len(pink_round)):
-    smallest_score = pink_round[i]
+for i in range(len(picture_percents)):
+    smallest_score = picture_percents[i]
     smallest_index = i
-
-    for j in range(i+1, len(pink_round)):
-        if pink_round[j] < smallest_score:
-            smallest_score < pink_round[j]
+    for j in range(i + 1, len(picture_percents)):
+        if picture_percents[j] > smallest_score:
+            smallest_score = picture_percents[j]
             smallest_index = j
-            pink_round[smallest_index], pink_round[i] = pink_round[i], pink_round[smallest_index]
-print(pink_round)
+    # switches to fulfill the list order
+    picture_percents[i], picture_percents[smallest_index] = (picture_percents[smallest_index], picture_percents[i])
 
-# binary search
-def search(list_of_lists, query):
+# binary search (for a list of numbers)
+def search(numbers, query):
     search_start_index = 0
-    search_end_index = len(list_of_lists) - 1
+    search_end_index = len(numbers) - 1
+
     while search_start_index <= search_end_index:
-        midpoint = int((search_start_index + search_end_index) / 2)
-        if list_of_lists[midpoint][0] == query:
-            return list_of_lists[midpoint][1]
-        elif list_of_lists[midpoint][0] < query:
+        midpoint = (search_start_index + search_end_index) // 2
+        if numbers[midpoint] == query:
+            return midpoint
+        elif numbers[midpoint] > query:
             search_start_index = midpoint + 1
         else:
             search_end_index = midpoint - 1
     return -1
+
+# shows top 5 pinkest pictures
+top_5 = picture_percents[:5]
+print("The top 5 pinkest pictures have these percentages: " + str(top_5))
+
+# user input to use binary search function
+print("Not just these 5 have been put into the list. It is in order from highest to lowest percentage of pink pixels.")
+finder = int(input("What percentage would you want to find the place of? "))
+results = search(picture_percents, finder) + 1
+
+# checks if in list or not
+if search(picture_percents, finder) == -1:
+    print("That percentage is not in the list.")
+else:
+    print("The percentage you wanted to find is in position: " + str(results))
